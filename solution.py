43ede990bf0313df38af82e237502ec1cb60ee46
Veli Kazan <deinemail@example.com>
2026-04-20 01:37:07 +0200

    test3

--- solution.py
+++ solution.py
@@ -1,13 +1,30 @@
 commits = [
     {"author": "Max", "message": "fix bug", "changes": 5},
     {"author": "Anna", "message": "add feature", "changes": 3},
-    {"author": "Max", "message": "fix login", "changes": 7}
+    {"author": "Max", "message": "fix login", "changes": 7},
+    {"author": "Tom", "message": "update readme", "changes": 2}
 ]
 
-count = 0
+n = 0
 
-for c in commits:
-    if "fix" in c["message"]:
-        count += 1
+for i in commits:
+    if "fix" in i["message"]:
+        n += 1
 
-print(count)
+print(n)
+
+changeperauthore = {"Max": 0, "Anna": 0, "Tom": 0}
+
+for i in commits:
+    if i["author"] == "Max":
+        changeperauthore["Max"] += 1
+
+    elif i["author"] == "Anna":
+        changeperauthore["Anna"] += 1
+
+    elif i["author"] == "Tom":
+        changeperauthore["Tom"] += 1
+
+print(changeperauthore)
+
+
